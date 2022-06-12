d8ca62f3de3861c6c0ea6b35422c1e91a72b13ee
Martin <martinkabe@seznam.cz>
2022-06-12 23:19:23 +0200

    Add tab panels approach

--- app/main.py
+++ app/main.py
@@ -1,16 +1,32 @@
+from flask import current_app as app
 from flask import Flask, render_template
 
-app = Flask(__name__,
-            template_folder='/opt/app/templates',
-            static_folder='/opt/app/static')
+app = Flask(
+    __name__,
+    template_folder='./templates',
+    static_folder='./static'
+)
 
-@app.route('/')
-def home(): 
-    return render_template('home.html')
 
+# index / home page...
+@app.route('/') 
+def home():
+	return render_template("index.html")
+
+# about us page..
 @app.route('/about')
 def about():
-    return render_template('about.html')
+	return render_template("about.html")
+
+# contact us page..
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
+
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int("8080"), debug=False) 
+    app.run(
+        host='0.0.0.0',
+        port=int("8080"),
+        debug=True
+    ) 
--- app/static/script.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('message').innerText = "Hello World From JavaScript!";
-});
